fa712d20959a29538bf204456205935cee0f0cd2
Tulioleal <tuliolealdev@gmail.com>
2025-01-06 19:05:36 -0300

    Add Plaiceholder integration and configure image optimization settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import withPlaiceholder from "@plaiceholder/next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+export default withPlaiceholder(nextConfig);
