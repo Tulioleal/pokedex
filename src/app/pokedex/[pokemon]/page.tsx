989ffce8aae97235a608ee16e8c1f77af9039d87
Tulioleal <tuliolealdev@gmail.com>
2025-01-11 00:09:58 -0300

    Enhance PokemonPage layout and functionality; add size comparison section, improve image handling, and implement type links for better navigation

--- src/app/pokedex/[pokemon]/page.tsx
+++ src/app/pokedex/[pokemon]/page.tsx
@@ -5,6 +5,8 @@ import styles from "./pokemonPage.module.scss";
 import Image from "next/image";
 import { UNOWN_IMAGE } from "@/lib/const";
 import LineChart from "@/components/StatsChart/StatsChart";
+import Link from "next/link";
+import TrainerRed from "../../../../public/trainer_red.png";
 
 export async function generateStaticParams() {
   const slugs = await getPokemonSlugs();
@@ -22,25 +24,43 @@ export default async function PokemonPage({
   
   return (
     <>
-      <h1>{capitalizePokemonName(pokemon.name)}</h1>
-      <section className={styles.container}>
-        <figure
-          className={`
-            ${styles.image}
-            ${styles[pokemon.types.map((type) => type.type.name).join("-")]}`
-          }
-        >
-          <Image
-            src={
-              pokemon.sprites.other["official-artwork"].front_default ||
-              pokemon.sprites.front_default ||
-              UNOWN_IMAGE // Default image (Unown)
+      <main className={styles.container}>
+        <section className={styles.imageContainer}>
+          <h1>{capitalizePokemonName(pokemon.name)}</h1>
+          <figure
+            className={`
+              ${styles.image}
+              ${styles[pokemon.types.map((type) => type.type.name).join("-")]}`
             }
-            width={280}
-            height={280}
-            alt={pokemon.name}
-          />
-        </figure>
+          >
+            <Image
+              src={
+                pokemon.sprites.other["official-artwork"].front_default ||
+                pokemon.sprites.front_default ||
+                UNOWN_IMAGE // Default image (Unown)
+              }
+              width={280}
+              height={280}
+              alt={pokemon.name}
+            />
+          </figure>
+          <div className={styles.types}>
+            {
+              pokemon.types.map((type) => (
+                <span  key={type.type.name}
+                    className={`${styles.type} ${styles[type.type.name + "-pill"]}`}
+                  >
+                  <Link href={`/types/${type.type.name}`} >
+                      {type.type.name.toUpperCase()}
+                  </Link>
+                </span>
+              ))
+            }
+          </div>
+          <p className={styles.description}>
+            {pokemon.location_area_encounters}
+          </p>
+        </section>
         <LineChart
           hp={pokemon.stats[0].base_stat}
           attack={pokemon.stats[1].base_stat}
@@ -50,12 +70,38 @@ export default async function PokemonPage({
           speed={pokemon.stats[5].base_stat}
           label={`${capitalizePokemonName(pokemon.name)} stats`}
         />
-      </section>
-      <ul>
-        {
-          JSON.stringify(pokemon)
-        }
-      </ul>
+        <section className={styles.sizeComparisonCOntainer}>
+          <h2>Size comparison</h2>
+          <div className={styles.sizeComparison}>
+            <figure>
+              <Image
+                src={pokemon.sprites.other.showdown.front_default || UNOWN_IMAGE}
+                width={pokemon.height > 38 ? 380 : pokemon.height * 10}
+                height={pokemon.height > 38 ? 380 : pokemon.height * 10}
+                alt={pokemon.name}
+              />
+            </figure>
+            <figure>
+              <Image
+                src={TrainerRed}
+                width={94}
+                height={160}
+                alt="Trainer Red"
+              />
+            </figure>
+          </div>
+          <div className={styles.sizes}>
+            <div className={styles.sizeComparisonItem}>
+              <p>Height</p>
+              <p>{pokemon.height}</p>
+            </div>
+            <div className={styles.sizeComparisonItem}>
+              <p>Weight</p>
+              <p>{pokemon.weight}</p>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
