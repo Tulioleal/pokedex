3642601dd75cd6c73bf5cf62256f6cf7a2b50ab3
Tulioleal <tuliolealdev@gmail.com>
2025-01-14 17:00:14 -0300

    Add TypeScript interfaces for Pokemon, Species, and related types to enhance type safety and structure

---
+++ src/interfaces.ts
@@ -0,0 +1,271 @@
+import { ability, cry, gameIndex, generalLink, statInfo, type } from "./types";
+
+interface HeldItem {
+  item: generalLink;
+  version_details: {
+    rarity: number;
+    version: generalLink;
+  }[];
+}
+
+interface Move {
+  move: generalLink;
+  version_group_details: Array<{
+    level_learned_at: number;
+    move_learn_method: generalLink;
+    version_group: generalLink;
+  }>;
+}
+
+export interface Sprite {
+  back_default: string | null;
+  back_female: string | null;
+  back_shiny: string | null;
+  back_shiny_female: string | null;
+  front_default: string | null;
+  front_female: string | null;
+  front_shiny: string | null;
+  front_shiny_female: string | null;
+  other: {
+    dream_world: {
+      front_default: string | null;
+      front_female: string | null;
+    };
+    home: {
+      front_default: string | null;
+      front_female: string | null;
+      front_shiny: string | null;
+      front_shiny_female: string | null;
+    };
+    "official-artwork": {
+      front_default: string;
+      front_shiny: string;
+    };
+    showdown: {
+      back_default: string | null;
+      back_female: string | null;
+      back_shiny: string | null;
+      back_shiny_female: string | null;
+      front_default: string | null;
+      front_female: string | null;
+      front_shiny: string | null;
+      front_shiny_female: string | null;
+    };
+  };
+  versions: {
+    "generation-i": {
+      "red-blue": {
+        back_default: string;
+        back_gray: string;
+        back_transparent: string;
+        front_default: string;
+        front_gray: string;
+        front_transparent: string;
+      };
+      yellow: {
+        back_default: string;
+        back_gray: string;
+        back_transparent: string;
+        front_default: string;
+        front_gray: string;
+        front_transparent: string;
+      };
+    };
+    "generation-ii": {
+      crystal: {
+        back_default: string;
+        back_shiny: string;
+        back_shiny_transparent: string;
+        back_transparent: string;
+        front_default: string;
+        front_shiny: string;
+        front_shiny_transparent: string;
+        front_transparent: string;
+      };
+      gold: {
+        back_default: string;
+        back_shiny: string;
+        front_default: string;
+        front_shiny: string;
+        front_transparent: string;
+      };
+      silver: {
+        back_default: string;
+        back_shiny: string;
+        front_default: string;
+        front_shiny: string;
+        front_transparent: string;
+      };
+    };
+    "generation-iii": {
+      emerald: {
+        front_default: string;
+        front_shiny: string;
+      };
+      "firered-leafgreen": {
+        back_default: string;
+        back_shiny: string;
+        front_default: string;
+        front_shiny: string;
+      };
+      "ruby-sapphire": {
+        back_default: string;
+        back_shiny: string;
+        front_default: string;
+        front_shiny: string;
+      };
+    };
+    "generation-iv": {
+      "diamond-pearl": {
+        back_default: string;
+        back_female: string | null;
+        back_shiny: string;
+        back_shiny_female: string | null;
+        front_default: string;
+        front_female: string | null;
+        front_shiny: string;
+        front_shiny_female: string | null;
+      };
+      "heartgold-soulsilver": {
+        back_default: string;
+        back_female: string | null;
+        back_shiny: string;
+        back_shiny_female: string | null;
+        front_default: string;
+        front_female: string | null;
+        front_shiny: string;
+        front_shiny_female: string | null;
+      };
+      platinum: {
+        back_default: string;
+        back_female: string | null;
+        back_shiny: string;
+        back_shiny_female: string | null;
+        front_default: string;
+        front_female: string | null;
+        front_shiny: string;
+        front_shiny_female: string | null;
+      };
+    };
+    "generation-v": {
+      "black-white": {
+        animated: {
+          back_default: string;
+          back_female: string | null;
+          back_shiny: string;
+          back_shiny_female: string | null;
+          front_default: string;
+          front_female: string | null;
+          front_shiny: string;
+          front_shiny_female: string | null;
+        };
+        back_default: string;
+        back_female: string | null;
+        back_shiny: string;
+        back_shiny_female: string | null;
+        front_default: string;
+        front_female: string | null;
+        front_shiny: string;
+        front_shiny_female: string | null;
+      };
+    };
+    "generation-vi": {
+      "omegaruby-alphasapphire": {
+        front_default: string;
+        front_female: string | null;
+        front_shiny: string;
+        front_shiny_female: string | null;
+      };
+      "x-y": {
+        front_default: string;
+        front_female: string | null;
+        front_shiny: string;
+        front_shiny_female: string | null;
+      };
+    };
+    "generation-vii": {
+      icons: {
+        front_default: string;
+        front_female: string | null;
+      };
+      "ultra-sun-ultra-moon": {
+        front_default: string;
+        front_female: string | null;
+        front_shiny: string;
+        front_shiny_female: string | null;
+      };
+    };
+    "generation-viii": {
+      icons: {
+        front_default: string;
+        front_female: string | null;
+      };
+    };
+  };
+}
+
+interface Pokemon {
+  abilities: ability[];
+  base_experience: number;
+  cries: cry;
+  forms: generalLink[];
+  game_indices: gameIndex[];
+  height: number;
+  held_items: HeldItem[];
+  id: number;
+  is_default: boolean;
+  location_area_encounters: string;
+  moves: Move[];
+  name: string;
+  order: number;
+  past_abilities: unknown[];
+  past_types: unknown[];
+  species: generalLink;
+  sprites: Sprite;
+  stats: statInfo[];
+  types: type[];
+  weight: number;
+}
+
+interface Species {
+  base_happiness: number;
+  capture_rate: number;
+  color: generalLink;
+  egg_groups: generalLink[];
+  evolution_chain: generalLink;
+  evolves_from_species: null | Species;
+  form_descriptions: string[];
+  forms_switchable: boolean;
+  gender_rate: number;
+  generation: generalLink;
+  growth_rate: generalLink;
+  habitat: generalLink;
+  has_gender_differences: boolean;
+  hatch_counter: number;
+  id: number;
+  is_baby: boolean;
+  is_legendary: boolean;
+  is_mythical: boolean;
+  name: string;
+  order: number;
+  pal_park_area: generalLink;
+  pokedex_numbers: number[];
+  shape: generalLink;
+  varieties: {
+    is_default: boolean;
+    pokemon: generalLink;
+  }[];
+}
+
+interface PokemonListResponse {
+  "count": number,
+  "next": null,
+  "previous": null,
+  "results": generalLink[]
+}
+
+export type {
+  Pokemon,
+  Species,
+  PokemonListResponse
+}
--- src/types.ts
+++ src/types.ts
@@ -1,271 +1,4 @@
-type PokemonListResponse = {
-  "count": number,
-  "next": null,
-  "previous": null,
-  "results": PokemonSimple[]
-}
-
-type PokemonSimple = {
-  name: string,
-  url: string
-}
-
-interface Ability {
-  ability: {
-    name: string;
-    url: string;
-  };
-  is_hidden: boolean;
-  slot: number;
-}
-
-interface Cry {
-  latest: string;
-  legacy: string;
-}
-
-interface Form {
-  name: string;
-  url: string;
-}
-
-interface GameIndex {
-  game_index: number;
-  version: {
-    name: string;
-    url: string;
-  };
-}
-
-interface HeldItem {
-  item: {
-    name: string;
-    url: string;
-  };
-  version_details: Array<{
-    rarity: number;
-    version: {
-      name: string;
-      url: string;
-    };
-  }>;
-}
-
-interface Move {
-  move: {
-    name: string;
-    url: string;
-  };
-  version_group_details: Array<{
-    level_learned_at: number;
-    move_learn_method: {
-      name: string;
-      url: string;
-    };
-    version_group: {
-      name: string;
-      url: string;
-    };
-  }>;
-}
-
-export interface Sprite {
-  back_default: string | null;
-  back_female: string | null;
-  back_shiny: string | null;
-  back_shiny_female: string | null;
-  front_default: string | null;
-  front_female: string | null;
-  front_shiny: string | null;
-  front_shiny_female: string | null;
-  other: {
-    dream_world: {
-      front_default: string | null;
-      front_female: string | null;
-    };
-    home: {
-      front_default: string | null;
-      front_female: string | null;
-      front_shiny: string | null;
-      front_shiny_female: string | null;
-    };
-    "official-artwork": {
-      front_default: string;
-      front_shiny: string;
-    };
-    showdown: {
-      back_default: string | null;
-      back_female: string | null;
-      back_shiny: string | null;
-      back_shiny_female: string | null;
-      front_default: string | null;
-      front_female: string | null;
-      front_shiny: string | null;
-      front_shiny_female: string | null;
-    };
-  };
-  versions: {
-    "generation-i": {
-      "red-blue": {
-        back_default: string;
-        back_gray: string;
-        back_transparent: string;
-        front_default: string;
-        front_gray: string;
-        front_transparent: string;
-      };
-      yellow: {
-        back_default: string;
-        back_gray: string;
-        back_transparent: string;
-        front_default: string;
-        front_gray: string;
-        front_transparent: string;
-      };
-    };
-    "generation-ii": {
-      crystal: {
-        back_default: string;
-        back_shiny: string;
-        back_shiny_transparent: string;
-        back_transparent: string;
-        front_default: string;
-        front_shiny: string;
-        front_shiny_transparent: string;
-        front_transparent: string;
-      };
-      gold: {
-        back_default: string;
-        back_shiny: string;
-        front_default: string;
-        front_shiny: string;
-        front_transparent: string;
-      };
-      silver: {
-        back_default: string;
-        back_shiny: string;
-        front_default: string;
-        front_shiny: string;
-        front_transparent: string;
-      };
-    };
-    "generation-iii": {
-      emerald: {
-        front_default: string;
-        front_shiny: string;
-      };
-      "firered-leafgreen": {
-        back_default: string;
-        back_shiny: string;
-        front_default: string;
-        front_shiny: string;
-      };
-      "ruby-sapphire": {
-        back_default: string;
-        back_shiny: string;
-        front_default: string;
-        front_shiny: string;
-      };
-    };
-    "generation-iv": {
-      "diamond-pearl": {
-        back_default: string;
-        back_female: string | null;
-        back_shiny: string;
-        back_shiny_female: string | null;
-        front_default: string;
-        front_female: string | null;
-        front_shiny: string;
-        front_shiny_female: string | null;
-      };
-      "heartgold-soulsilver": {
-        back_default: string;
-        back_female: string | null;
-        back_shiny: string;
-        back_shiny_female: string | null;
-        front_default: string;
-        front_female: string | null;
-        front_shiny: string;
-        front_shiny_female: string | null;
-      };
-      platinum: {
-        back_default: string;
-        back_female: string | null;
-        back_shiny: string;
-        back_shiny_female: string | null;
-        front_default: string;
-        front_female: string | null;
-        front_shiny: string;
-        front_shiny_female: string | null;
-      };
-    };
-    "generation-v": {
-      "black-white": {
-        animated: {
-          back_default: string;
-          back_female: string | null;
-          back_shiny: string;
-          back_shiny_female: string | null;
-          front_default: string;
-          front_female: string | null;
-          front_shiny: string;
-          front_shiny_female: string | null;
-        };
-        back_default: string;
-        back_female: string | null;
-        back_shiny: string;
-        back_shiny_female: string | null;
-        front_default: string;
-        front_female: string | null;
-        front_shiny: string;
-        front_shiny_female: string | null;
-      };
-    };
-    "generation-vi": {
-      "omegaruby-alphasapphire": {
-        front_default: string;
-        front_female: string | null;
-        front_shiny: string;
-        front_shiny_female: string | null;
-      };
-      "x-y": {
-        front_default: string;
-        front_female: string | null;
-        front_shiny: string;
-        front_shiny_female: string | null;
-      };
-    };
-    "generation-vii": {
-      icons: {
-        front_default: string;
-        front_female: string | null;
-      };
-      "ultra-sun-ultra-moon": {
-        front_default: string;
-        front_female: string | null;
-        front_shiny: string;
-        front_shiny_female: string | null;
-      };
-    };
-    "generation-viii": {
-      icons: {
-        front_default: string;
-        front_female: string | null;
-      };
-    };
-  };
-}
-
-export interface Stat {
-  base_stat: number;
-  effort: number;
-  stat: {
-    name: string;
-    url: string;
-  };
-}
-
-export type PokemonType =
+type pokemonType =
   | 'normal'
   | 'fire'
   | 'water'
@@ -285,7 +18,23 @@ export type PokemonType =
   | 'steel'
   | 'fairy';
 
-export type stat = {
+type generalLink = {
+  name: string;
+  url: string;
+}
+
+interface ability {
+  ability: generalLink;
+  is_hidden: boolean;
+  slot: number;
+}
+
+interface cry {
+  latest: string;
+  legacy: string;
+}
+
+type stat = {
   hp: number;
   attack: number;
   defense: number;
@@ -294,38 +43,36 @@ export type stat = {
   speed: number;
 }
 
-export interface Type {
+type type = {
   slot: number;
-  type: {
-    name: PokemonType;
-    url: string;
-  };
+  type: generalLink;
 }
 
-interface Pokemon {
-  abilities: Ability[];
-  base_experience: number;
-  cries: Cry;
-  forms: Form[];
-  game_indices: GameIndex[];
-  height: number;
-  held_items: HeldItem[];
-  id: number;
-  is_default: boolean;
-  location_area_encounters: string;
-  moves: Move[];
-  name: string;
-  order: number;
-  past_abilities: unknown[];
-  past_types: unknown[];
-  species: {
-    name: string;
-    url: string;
-  };
-  sprites: Sprite;
-  stats: Stat[];
-  types: Type[];
-  weight: number;
+type statInfo = {
+  base_stat: number;
+  effort: number;
+  stat: generalLink;
 }
 
-export type { PokemonListResponse, PokemonSimple, Pokemon };
+
+interface gameIndex {
+  game_index: number;
+  version: generalLink;
+}
+
+type versionDetail = {
+  rarity: number;
+  version: generalLink;
+}
+
+export type {
+  ability,
+  cry,
+  generalLink,
+  pokemonType,
+  stat,
+  statInfo,
+  type,
+  gameIndex,
+  versionDetail
+}
