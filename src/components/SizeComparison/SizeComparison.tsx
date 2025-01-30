e3879780f00e42f97b2f5967caaa0f1a4e735f43
Tulioleal <tuliolealdev@gmail.com>
2025-01-30 18:54:27 -0300

    Refactor SizeComparison component: update styles for better layout and adjust image source handling for optional chaining

--- src/components/SizeComparison/SizeComparison.tsx
+++ src/components/SizeComparison/SizeComparison.tsx
@@ -3,9 +3,9 @@
 import Image from "next/image";
 import styles from "./SizeComparison.module.scss";
 import { FC, useState } from "react";
-import { Pokemon } from "@/types";
 import { UNOWN_IMAGE } from "@/lib/const";
 import TrainerRed from "../../../public/trainer_red.png";
+import { Pokemon } from "@/interfaces";
 
 const SizeComparison:FC<Pokemon> = (pokemon) => {
   const [imageWidth, setImageWidth] = useState({
@@ -28,7 +28,7 @@ const SizeComparison:FC<Pokemon> = (pokemon) => {
     <h2>Size comparison</h2>
     <div className={styles.sizeComparison}>
       <Image
-        src={pokemon.sprites.other.showdown.front_default || UNOWN_IMAGE}
+        src={pokemon.sprites.other?.showdown.front_default || UNOWN_IMAGE}
         width={0}
         height={0}
         alt={pokemon.name}
@@ -52,31 +52,6 @@ const SizeComparison:FC<Pokemon> = (pokemon) => {
           maxHeight: `${(pokemon.height > 38 ? (38 / pokemon.height) * 160 : 160) / imageWidth.aspectRatio}px`,
         }}
       />
-    </div>
-    <div className={styles.sizes}>
-      <div className={styles.sizeComparisonItem}>
-        <p>Height</p>
-        <p>
-          {
-            pokemon.height * 10 > 100 ?
-              `${pokemon.height / 10} mts.` :
-              `${pokemon.height * 10} cm.`
-          }
-        </p>
-      </div>
-      <div className={styles.sizeComparisonItem}>
-        <p>Weight</p>
-        <p>
-          {
-            pokemon.weight * 100 < 1000 ?
-              `${pokemon.weight * 100} gr.` :
-              pokemon.weight * 100 >= 1000000 ?
-                `${pokemon.weight / 10000} t.` :
-                `${pokemon.weight / 10} kg.`
-
-          }
-        </p>
-      </div>
     </div>
   </section>
   );
