37bb13893db6a91f9927135e26f4ac24f63e2f8a
Tulioleal <tuliolealdev@gmail.com>
2025-01-30 18:54:37 -0300

    Add PokemonDescription component with animation and data fetching for flavor text

---
+++ src/components/PokemonDescription/PokemonDescription.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import getPokemonDescription from "@/db/getPokemonDescription";
+import styles from "./PokemonDescription.module.scss"
+import { TypeAnimation } from 'react-type-animation';
+import { useEffect, useState } from "react";
+import { FlavorText } from "pokenode-ts";
+
+const PokemonDescription = ({
+  id
+}: {
+  id:number
+}) => {
+  const [data, setData] = useState<FlavorText|null>(null);
+
+  useEffect(() => {
+    getPokemonDescription(id)
+      .then((res) => {
+        setData(res)
+      })
+  }, []);
+
+  if (!data) return <></>
+
+  return (
+    <span className={styles['letter-by-letter-container']}>
+    <TypeAnimation
+      style={{ whiteSpace: 'pre-line', height: '60px', display: 'inline-block' }}
+      sequence={[data.flavor_text]}
+      cursor={false}
+      
+    />
+  </span>
+  );
+}
+
+export default PokemonDescription;
