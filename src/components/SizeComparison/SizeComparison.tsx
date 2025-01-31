624abf52b993210bb6711886d977c75d1f6f2a07
Tulioleal <tuliolealdev@gmail.com>
2025-01-31 19:47:47 -0300

    Refactor SizeComparison component: enhance styles with dynamic type handling and improve image height calculations for responsiveness

--- src/components/SizeComparison/SizeComparison.tsx
+++ src/components/SizeComparison/SizeComparison.tsx
@@ -6,6 +6,10 @@ import { FC, useState } from "react";
 import { UNOWN_IMAGE } from "@/lib/const";
 import TrainerRed from "../../../public/trainer_red.png";
 import { Pokemon } from "@/interfaces";
+import useViewSize from "@/hooks/useViewSize";
+
+const RED_HEIGHT = 16;
+const MAX_HEIGHT = 38;
 
 const SizeComparison:FC<Pokemon> = (pokemon) => {
   const [imageWidth, setImageWidth] = useState({
@@ -13,6 +17,43 @@ const SizeComparison:FC<Pokemon> = (pokemon) => {
     height: 0,
     aspectRatio: 0,
   });
+  const {width} = useViewSize();
+
+  const getImageHeight = (pokemonHeight: number, isRed?:boolean):string => {
+    let maxHeight = MAX_HEIGHT;
+  
+    if (width < 768) {
+      maxHeight = MAX_HEIGHT * 0.75;
+    }
+  
+    if (width < 480) {
+      maxHeight = MAX_HEIGHT * 0.5;
+    }
+  
+    if ( isRed ) {
+      return `${pokemonHeight > maxHeight ? (maxHeight / pokemonHeight) * (RED_HEIGHT * 10) : (RED_HEIGHT * 10)}px`
+    }
+  
+    return `${pokemonHeight > maxHeight ? maxHeight * 10 : pokemonHeight * 10}px`
+  }
+  
+  const getImageMaxHigh = (pokemonHeight: number, aspectRatio:number ,isRed?:boolean):string => {
+    let maxHeight = MAX_HEIGHT;
+  
+    if (width < 768) {
+      maxHeight = MAX_HEIGHT * 0.75;
+    }
+  
+    else if (width < 480) {
+      maxHeight = MAX_HEIGHT * 0.5;
+    }
+  
+    if ( isRed ) {
+      return `${(pokemonHeight > maxHeight ? (maxHeight / pokemonHeight) * (RED_HEIGHT * 10) : (RED_HEIGHT * 10)) / aspectRatio}px`
+    }
+  
+    return `${(pokemonHeight > maxHeight ? maxHeight * 10 : pokemonHeight * 10) / aspectRatio}px`
+  }
 
   const handleImageLoad = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
     const { naturalWidth, naturalHeight } = event.currentTarget;
@@ -25,35 +66,39 @@ const SizeComparison:FC<Pokemon> = (pokemon) => {
 
   return (
     <section className={styles.sizeComparisonContainer}>
-    <h2>Size comparison</h2>
-    <div className={styles.sizeComparison}>
-      <Image
-        src={pokemon.sprites.other?.showdown.front_default || UNOWN_IMAGE}
-        width={0}
-        height={0}
-        alt={pokemon.name}
-        style={{
-          width: 'auto',
-          maxWidth: '380px',
-          height: `${pokemon.height > 38 ? 380 : pokemon.height * 10}px`,
-          filter: 'brightness(0.85)',
-          maxHeight: `${(pokemon.height > 38 ? 380 : pokemon.height * 10) / imageWidth.aspectRatio}px`,
-        }}
-        onLoad={handleImageLoad}
-      />
-      <Image
-        src={TrainerRed}
-        width={0}
-        height={0} // make it dynamic when pokemon is bigger than 380
-        alt="Trainer Red"
-        style={{
-          width: 'auto',
-          height: `${pokemon.height > 38 ? (38 / pokemon.height) * 160 : 160}px`,
-          maxHeight: `${(pokemon.height > 38 ? (38 / pokemon.height) * 160 : 160) / imageWidth.aspectRatio}px`,
-        }}
-      />
-    </div>
-  </section>
+      <h2>Size comparison</h2>
+      <div className={`
+        ${styles.sizeComparison}
+        ${styles[pokemon.types[0].type.name]}
+      `}
+      >
+        <Image
+          src={pokemon.sprites.other?.showdown.front_default || UNOWN_IMAGE}
+          width={0}
+          height={0}
+          alt={pokemon.name}
+          style={{
+            width: 'auto',
+            maxWidth: '380px',
+            filter: 'brightness(0.85)',
+            height: getImageHeight(pokemon.height),
+            maxHeight: getImageMaxHigh(pokemon.height, imageWidth.aspectRatio),
+          }}
+          onLoad={handleImageLoad}
+        />
+        <Image
+          src={TrainerRed}
+          width={0}
+          height={0} // make it dynamic when pokemon is bigger than 380
+          alt="Trainer Red"
+          style={{
+            width: 'auto',
+            height: getImageHeight(pokemon.height, true),
+            maxHeight: getImageMaxHigh(pokemon.height, imageWidth.aspectRatio, true),
+          }}
+        />
+      </div>
+    </section>
   );
 }
 
