36eab22eec57eaf73b1b8c342308488036fa218e
Tulioleal <tuliolealdev@gmail.com>
2025-02-05 18:51:36 -0300

    Comment out pagination logic in PokemonList component for future reference

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,31 +1,31 @@
 "use client"
 
 import { generalLink } from "@/types";
-import { useState } from "react";
+// import { useState } from "react";
 import styles from  "./PokemonList.module.css";
 import { PokemonCard } from "../PokemonCard/PokemonCard";
 
-const POKEMON_LIMIT = 50;
+// const POKEMON_LIMIT = 50;
 
 export const PokemonList = (
   { pokemonList } :
   { pokemonList: generalLink[] }
 ) => {
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const pages = Math.ceil(pokemonList.length / POKEMON_LIMIT);
+  // const [currentPage, setCurrentPage] = useState<number>(1);
+  // const pages = Math.ceil(pokemonList.length / POKEMON_LIMIT);
 
-  const handlePageChange = (nextPage:number) => {
+  // const handlePageChange = (nextPage:number) => {
 
-    if (nextPage > pages || nextPage < 1) {
-      return;
-    }
+  //   if (nextPage > pages || nextPage < 1) {
+  //     return;
+  //   }
 
-    setCurrentPage(nextPage);
-  }
+  //   setCurrentPage(nextPage);
+  // }
 
   return (
     <article>
-      <button
+      {/* <button
         disabled={currentPage - 1 < 1}
         onClick={() => handlePageChange(currentPage - 1)}
       >Prev</button>
@@ -33,10 +33,12 @@ export const PokemonList = (
       <button
         disabled={currentPage + 1 > pages}
         onClick={() => handlePageChange(currentPage + 1)}
-      >Next</button>
+      >Next</button> */}
       <div className={styles.pokemonList}>
         {
-          pokemonList.slice((currentPage * POKEMON_LIMIT) - POKEMON_LIMIT, currentPage * POKEMON_LIMIT).map((pokemon) => (
+          pokemonList
+          // .slice((currentPage * POKEMON_LIMIT) - POKEMON_LIMIT, currentPage * POKEMON_LIMIT)
+          .map((pokemon) => (
             <PokemonCard pokemon={pokemon} key={pokemon.name}/>
           ))
         } 
