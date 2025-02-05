e7c87b57b7672640b29d2bb18adcfde1115b65bd
Tulioleal <tuliolealdev@gmail.com>
2025-02-05 18:51:49 -0300

    Enhance PokedexWrapper component with open/close functionality and add light blue gradient styles

--- src/components/PokedexWrapper/PokedexWrapper.tsx
+++ src/components/PokedexWrapper/PokedexWrapper.tsx
@@ -1,28 +1,57 @@
+"use client"
+
+import { FC, ReactNode, useState } from 'react';
 import styles from './PokedexWrapper.module.scss';
 
-const PokedexWrapper = () => {
+interface PokedexWrapperProps {
+  children?: string | ReactNode
+  openDefault?: boolean
+}
+
+const PokedexWrapper:FC<PokedexWrapperProps> = ({
+  children,
+  openDefault = false
+}) => {
+  const [open, setOpen] = useState<boolean>(openDefault);
+
   return (
-    <div className={styles.pokedex}>
-      <div className={styles.top}>
-        <div className={`${styles.border} ${styles.black}`} />
-        <div className={`${styles.border} ${styles.clear}`} />
-        <div className={`${styles.border} ${styles.thick} ${styles.black}`} />
-        <div  className={`${styles.border} ${styles.blue}`} />
-        <div  className={`${styles.border} ${styles.thin} ${styles.dark_blue}`} />
-      </div>
-      <div className={styles.center}>
-        <div className={styles.line}>
-          <div>
-            
+    <div className={styles.container}>
+      <div className={`${styles.pokedex} ${open ? styles.open : ''}`}>
+        <div className={styles.top}>
+          <div className={`${styles.corner} ${styles.left}`} />
+          <div className={`${styles.corner} ${styles.right}`} />
+          <div className={`${styles.border} ${styles.black}`} />
+          <div className={`${styles.border} ${styles.clear}`} />
+          <div className={`${styles.border} ${styles.thick} ${styles.black}`} />
+          <div  className={`${styles.border} ${styles.blue}`} />
+        </div>
+        <div className={styles.center}>
+          <div className={styles.line}>
+            <div className={styles.content}>
+              {
+                open && <button className={`${styles.button} ${styles.close}`} onClick={() => setOpen(!open)}>
+                  Close
+                </button>
+              }
+              {
+                children
+              }
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.bottom}>
-        <div className={`${styles.border} ${styles.black}`} />
-        <div className={`${styles.border} ${styles.clear}`} />
-        <div className={`${styles.border} ${styles.thick} ${styles.black}`} />
-        <div  className={`${styles.border} ${styles.blue}`} />
-        <div  className={`${styles.border} ${styles.thin} ${styles.dark_blue}`} />
+        <div className={styles.bottom}>
+          <div className={`${styles.corner} ${styles.left}`} />
+          <div className={`${styles.corner} ${styles.right}`} />
+          <div className={`${styles.border} ${styles.black}`} />
+          <div className={`${styles.border} ${styles.clear}`} />
+          <div className={`${styles.border} ${styles.thick} ${styles.black}`} />
+          <div  className={`${styles.border} ${styles.blue}`} />
+        </div>
+        {
+          !open && <button className={styles.button} onClick={() => setOpen(!open)}>
+            Start
+          </button>
+        }
       </div>
     </div>
   );
