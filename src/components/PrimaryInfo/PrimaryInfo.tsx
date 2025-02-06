96fb265f409189b771dba46af1a5ad3318995262
Tulioleal <tuliolealdev@gmail.com>
2025-02-06 18:31:42 -0300

    Refactor PrimaryInfo component; extract PrimaryImage subcomponent for improved readability and maintainability, enhance hover effects and transitions in styles

--- src/components/PrimaryInfo/PrimaryInfo.tsx
+++ src/components/PrimaryInfo/PrimaryInfo.tsx
@@ -5,28 +5,17 @@ import styles from "./PrimaryInfo.module.scss"
 import { Pokemon } from "@/interfaces";
 import { capitalizePokemonName, getPokemonTypeClassName } from "@/lib/utils";
 import { pokemonType } from "@/types";
+import { FC } from "react";
 
 const PrimaryInfo = (pokemon:Pokemon) => {
   return (
     <section className={styles.container}>
       <h1>{capitalizePokemonName(pokemon.name)}</h1>
-      <figure
-        className={`
-          ${styles.image}
-          ${styles[getPokemonTypeClassName(pokemon.types.map((type) => type.type.name as pokemonType))]}
-        `}
-      >
-        <Image
-          src={
-            pokemon.sprites.other?.["official-artwork"].front_default ||
-            pokemon.sprites.front_default ||
-            UNOWN_IMAGE // Default image (Unown)
-          }
-          width={300}
-          height={300}
-          alt={pokemon.name}
-        />
-      </figure>
+      <PrimaryImage
+        name={pokemon.name}
+        image={pokemon.sprites.other["official-artwork"].front_default || UNOWN_IMAGE}
+        types={pokemon.types.map((type) => type.type.name as pokemonType)}
+      />
       <div className={styles.types}>
         {
           pokemon.types.map((type) => (
@@ -41,6 +30,34 @@ const PrimaryInfo = (pokemon:Pokemon) => {
       <PokemonDescription id={pokemon.id}/>
     </section>
   );
+}
+
+interface PrimaryImageProps {
+  name:string;
+  image:string;
+  types:pokemonType[];
+}
+
+const PrimaryImage:FC<PrimaryImageProps> = ({
+  name,
+  image,
+  types
+}) => {
+  return (
+    <figure
+      className={`
+        ${styles.image}
+        ${styles[getPokemonTypeClassName(types.map((type) => type as pokemonType))]}
+      `}
+    >
+      <Image
+        src={image}
+        width={300}
+        height={300}
+        alt={name}
+      />
+    </figure>
+  )
 }
 
 export default PrimaryInfo;
